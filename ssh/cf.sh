a0ef813d6e66802248c447e561702e0972565dd5
mousethain <mousethain@gmail.com>
2023-12-30 19:22:36 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -9,7 +9,7 @@ apt install jq curl -y >/dev/null 2>&1
 DOMAIN=red-flat.my.id
 sub=$(</dev/urandom tr -dc a-x1-9 | head -c5 | tr -d '\r' | tr -d '\r\n')
 SUB_DOMAIN=${sub}.red-flat.my.id
-CF_ID=wirogendeng980@gmail.com
+CF_ID=mousevpn@gmail.com
 CF_KEY=4ba725444dad4c14cfdcc144e9cd9b26177fc
 set -euo pipefail
 IP=$(wget -qO- ifconfig.me/ip);
