6748ea2a267d7b73d0ac4ec2efb064cb1c6bf2cc
mousethain <mousethain@gmail.com>
2024-01-06 23:09:31 +0700

    Update menu.py

--- bot/modules/menu.py
+++ bot/modules/menu.py
@@ -10,7 +10,7 @@ Button.inline(" VLESS MANAGER ","vless")],
 [Button.inline(" TROJAN MANAGER ","trojan")],
 [Button.inline(" OTHER SETTING ","setting")]]
 	ox = requests.get(f"https://ipv4.icanhazip.com").text.strip()
-	bz = f" curl -sS https://raw.githubusercontent.com/CatzBurry/AIO/main/access/ip | grep '{ox}'| cut -d ' ' -f6 "
+	bz = f" curl -sS https://raw.githubusercontent.com/mousethain/penak/main/access/ip | grep '{ox}'| cut -d ' ' -f6 "
 	bo = subprocess.check_output(bz, shell=True).decode("ascii").strip()
 	if not ox != bo:
 		sender = await event.get_sender()
